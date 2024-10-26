05fd61715bb8c6a3023448463a8b84f6e61201f9
Amrit Krishn <amritkrishn06072003@gmail.com>
2024-10-26 23:19:13 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const nodemailer = require('nodemailer');
+require('dotenv').config(); // Load environment variables from .env file
+
+const app = express();
+
+// View engine setup
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Static folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.render('contact');
+});
+
+app.post('/send', (req, res) => {
+  const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>  
+      <li>Name: ${req.body.name}</li>
+      <li>Company: ${req.body.company}</li>
+      <li>Email: ${req.body.email}</li>
+      <li>Phone: ${req.body.phone}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+  `;
+
+  // Create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.SMTP_USER, // Your email
+      pass: process.env.SMTP_PASS  // Your email password or app password
+    },
+    tls: {
+      rejectUnauthorized: false
+    }
+  });
+
+  // Setup email data
+  let mailOptions = {
+    from: `"Nodemailer Contact" <${process.env.SMTP_USER}>`, // sender address
+    to: 'sajalkumar2711@gmail.com', // receiver
+    subject: 'Node Contact Request', // Subject line
+    text: 'Hello world?', // plain text body
+    html: output // html body
+  };
+
+  // Send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message sent: %s', info.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+    res.render('contact', { msg: 'Email has been sent' });
+  });
+});
+
+app.listen(3000, () => console.log('Server started on http://localhost:3000'));
